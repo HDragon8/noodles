40d188543cadad8f0e38ce2fe4a2d83cfa3fe5f4
HDragon <houyonghua2015@gmail.com>
2020-03-09 16:03:25 +0800

    Add files via upload

---
+++ diy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname
+sed -i 's/OpenWrt/noodles/g' package/base-files/files/bin/config_generate
+
+# Modify default WiFi SSID
+sed -i 's/OpenWrt/noodles/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
